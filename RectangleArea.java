f642b730b84a87128842a14c4ec878dc7292dcc5
deepak-singh08 <deepakthakur.123123@gmail.com>
2026-04-13 11:24:15 +0530

    area code

---
+++ RectangleArea.java
@@ -0,0 +1,10 @@
+class RectangleArea {
+    public static void main(String[] args) {
+
+        int l = 10, b = 5;
+
+        int area = l * b;
+
+        System.out.println("Area: " + area);
+    }
+}
