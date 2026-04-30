489c72ea9dca70ca99f5c6e7fd9be77b01589ce7
deepak-singh08 <deepakthakur.123123@gmail.com>
2026-04-30 13:27:34 +0530

    commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+      try {
+        
+      } catch (Exception e) {
+        // TODO: handle exception
+      }  int result = 10 / 0;
+        System.out.println(result);
+    }
+}
